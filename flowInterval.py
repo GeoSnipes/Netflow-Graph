a617479c3581eafe8056da5bba598c809a9554ff
GeoSnipes <geovanni.fortisyute@gmail.com>
2018-01-29 17:53:02 -0600

    V2.0
    Adding the ability for program to seperate each flow depending on  interval used (if flow last longer than interval, then create new flow with remaining info)

--- flowInterval.py
+++ flowInterval.py
@@ -2,6 +2,8 @@
 import re
 import time
 import datetime
+from math import floor, ceil
+from decimal import Decimal, getcontext
 
 class NetFlow:
 
@@ -34,7 +36,7 @@ class NetFlow:
                 num = int(float(found[:-2]) * 10 ** 6)
                 line = line.replace(found, str(num))
 
-        expr = re.compile("\d+\.\d+\s{1}G")  # pattern for gigabytes
+        expr = re.compile("\d+\.\d+\s{1}G(?!RE)")  # pattern for gigabytes, ignoring when protocol is GRE
         result = expr.findall(line)
         if len(result) > 0:
             for found in result:
@@ -45,7 +47,7 @@ class NetFlow:
 
     def __getData(self):
         """time_since_epoch[0] Duration[1] Proto[2] Src_IP_Addr:Port[3] Dir[4] Dst_IP_Addr:Port[5] Flags[6] Tos[7] Packets[8] Bytes[9] pps[10] bps[11] Bpp[12] Flows[13]"""
-        entries=[]
+        entries = []
         with open("formatted_nfcapd.txt") as file:
             file.readline()                                 #skip the first line
             for line in file:
@@ -68,13 +70,48 @@ class NetFlow:
         dateEntry[0] = date
         return dateEntry
 
+    def __format_long_transmits(self, data):
+        """For transfer that last longer than the intervals"""
+        getcontext().prec = 4
+        beg = 0     # used to save processing time, instead of going over list each time, start at point where list ends
+        allChecked = True
+        newData = []        # save new flows that last longer than interval
+        while allChecked:     # for does not iterate through an ever updating list, so have to refresh the iteration counter
+            allChecked = False
+            for pos in range(beg, len(data)):
+                beg += 1
+                if float(data[pos][1]) > self.interval:
+                    allChecked = True         # then there is a flow that last longer than interval
+                    flowDuration = data[pos][1]
+                    tempFlow = list(data[pos])
+                    data[pos][1] = str(self.interval)   # if flowDuration was greater than interval, then set it to interval
+                    for val in range(8, 13):            # calculate ratio of val:flowDuration
+                        if val != 10:
+                            data[pos][val] = str(floor(float(data[pos][val]) / float(flowDuration) * self.interval))
+                        else:
+                            data[pos][val] = str(ceil(float(data[pos][val]) / float(flowDuration) * self.interval))     # can't have 0 packets
+                    self.__remain(tempFlow, data[pos])
+                    newData.append(tempFlow)
+
+            data = data + newData
+            newData = []
+        return data
+
+    def __remain(self, tempFlow, flow):
+        """Calculate remaining data from interval-ed original"""
+        tempFlow[0] = tempFlow[0] + self.interval
+        tempFlow[1] = str(float(Decimal(tempFlow[1]) - Decimal(self.interval)))
+        for pos in range(8,13):
+            tempFlow[pos] = str(ceil(Decimal(tempFlow[pos]) - Decimal(flow[pos])))
+
     def __cal_interval(self, data):
+        """Summation of each interval bytes transfered"""
         finalSumList = []
         cumSum = 0
         start = data[0][0]
         finish = start + self.interval
         for i in range(len(data)):
-            if data[i][0] <= finish:                          #value is less than finish
+            if data[i][0] < finish:                          #value is less than finish
                 try:
                     cumSum += int(data[i][9])
                 except ValueError as e:
@@ -84,7 +121,7 @@ class NetFlow:
                 finalSumList.append(cumSum)                 #update final list of entries
                 start = finish                               #update new interval
                 finish += self.interval
-                if start <= data[i][0] <= finish:  #if current entry is within new interval continue
+                if start <= data[i][0] < finish:  #if current entry is within new interval continue
                     try:
                         cumSum = int(data[i][9])
                     except ValueError as e:
@@ -92,7 +129,7 @@ class NetFlow:
                         print(data[i])
                     continue
                 else:
-                    while not (start <= data[i][0] <= finish):           #if current entry is not within new interval then append 0 to final list until it true
+                    while not (start <= data[i][0] < finish):           #if current entry is not within new interval then append 0 to final list until it true
                         finalSumList.append(0)
                         start = finish
                         finish += self.interval
@@ -121,17 +158,18 @@ class NetFlow:
     def run(self):
         self.__formatFile()
         data = self.__getData()
+        data = self.__format_long_transmits(data)
         data.sort(key=self.__get_first)  # sort by first element in each sublist
         intervalList = self.__cal_interval(data)
         self.__output_interval(intervalList, data[0][0], data[len(data) - 1][0])
 
-#origFile = ["netflowExtractedFiles/nfcapd.201801100000cswitch1.txt", "netflowExtractedFiles/nfcapd.201801100000cswitch2.txt", "netflowExtractedFiles/nfcapd.201801100000cswitch3.txt", "netflowExtractedFiles/nfcapd.201801100000dcdist.txt", "netflowExtractedFiles/nfcapd.201801100000igate.txt"]
-#finalOutput = ["netflowFinalised/cswitch1.201801100000.txt", "netflowFinalised/cswitch2.201801100000.txt", "netflowFinalised/cswitch3.201801100000.txt", "netflowFinalised/dcdist.201801100000.txt", "netflowFinalised/igate.201801100000.txt"]
-origFile = ["netflowExtractedFiles/testFile.txt"]
-finalOutput = ["netflowFinalised/testFile.txt"]
+origFile = ["netflowExtractedFiles/nfcapd.201801100000cswitch1.txt", "netflowExtractedFiles/nfcapd.201801100000cswitch2.txt", "netflowExtractedFiles/nfcapd.201801100000cswitch3.txt", "netflowExtractedFiles/nfcapd.201801100000dcdist.txt", "netflowExtractedFiles/nfcapd.201801100000igate.txt"]
+finalOutput = ["netflowFinalised/cswitch1.201801100000.txt", "netflowFinalised/cswitch2.201801100000.txt", "netflowFinalised/cswitch3.201801100000.txt", "netflowFinalised/dcdist.201801100000.txt", "netflowFinalised/igate.201801100000.txt"]
+#origFile = ["netflowExtractedFiles/testFile.txt"]
+#finalOutput = ["netflowFinalised/testFile.txt"]
 nF = []
 
-interval = 5
+interval = 10
 
 startTimer = time.time()
 
